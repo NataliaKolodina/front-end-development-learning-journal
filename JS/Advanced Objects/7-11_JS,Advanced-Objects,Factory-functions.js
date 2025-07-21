301d0d822d700f5734dabf3798e1a36346ca1f6a
Nataly <kolodina_melbyk@mail.ru>
2025-07-21 18:15:47 +0400

    Update 7-11_JS,Advanced-Objects,Factory-functions.js

--- JS/Advanced Objects/7-11_JS,Advanced-Objects,Factory-functions.js
+++ JS/Advanced Objects/7-11_JS,Advanced-Objects,Factory-functions.js
@@ -35,3 +35,8 @@ const robotFactory = (id, model, energyLevel, speakMessage) => {
 const robot1 = robotFactory(101, 'XR-22', 80, 'Mission ready!');
 robot1.speak();
 console.log(robot1.energyLevel);
+
+const robot2 = robotFactory(202, 'MK-9', 45, 'Low battery...');
+robot2.speak();
+console.log(robot2.model);
+console.log(robot2.id);
