96933ac0207773e9ef9ec72d9898057d4447bace
Nataly <kolodina_melbyk@mail.ru>
2025-07-30 15:37:27 +0400

    Update 8-11_JS,Advanced-Objects,Property-Value-Shorthand.js

--- JS/Advanced Objects/8-11_JS,Advanced-Objects,Property-Value-Shorthand.js
+++ JS/Advanced Objects/8-11_JS,Advanced-Objects,Property-Value-Shorthand.js
@@ -15,3 +15,17 @@ ESERCIZIO 2: Creare un secondo animale
 3. Stampa `kangaroo.species`. // Risultato atteso: Canguro
 4. Stampa `kangaroo.legs`. // Risultato atteso: 2
 ====================================== */
+
+function animalFactory(species, legs) {
+  return {
+    species,
+    legs,
+  };
+}
+const giraffe = animalFactory("Giraffa", 4);
+console.log(giraffe.species);
+console.log(giraffe.legs);
+
+const kangaroo = animalFactory("Canguro", 2);
+console.log(kangaroo.species);
+console.log(kangaroo.legs);
